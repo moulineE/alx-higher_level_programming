bdd67b8c204ae623dc7febd69c0a4a80c7bbf6ab
moulineE <elmahdi.mouline@live.fr>
2023-06-07 06:32:58 +0000

    Task 8. To uppercase FIX: use asingle time the function print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     """prints a string in uppercase followed by a new line."""
+    rs = ""
     for char in str:
         if ord(char) > 96 and ord(char) < 123:
             char = chr(ord(char) - 32)
-        print("{}".format(char), end="")
+        rs = rs + char
 
-    print("{}".format(""))
+    print("{}".format(rs))
