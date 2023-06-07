391b5f2ffad883460c8f3084ad080e20bb9bcaea
moulineE <elmahdi.mouline@live.fr>
2023-06-07 05:34:29 +0000

    Task 8. To uppercase FIX test 6

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     """prints a string in uppercase followed by a new line."""
-    for c in str:
-        if ord(c) > 96 and ord(c) < 123:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
+    for char in str:
+        if ord(char) > 96 and ord(char) < 123:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
 
-    print()
+    print("{}".format(""))
