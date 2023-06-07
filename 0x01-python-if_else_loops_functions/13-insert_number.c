a69bdb22ec6e439cb8615af25cf1277858245e62
moulineE <elmahdi.mouline@live.fr>
2023-06-07 19:20:20 +0000

    Task 13. Insert in sorted linked list FIX case: empty list and insert node at the beginning == fixing head = NULL if statement

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current, *new;
 
 	current = *head;
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
