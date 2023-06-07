e789395a9705d72c3c32d65add8c1ad51bbbd695
moulineE <elmahdi.mouline@live.fr>
2023-06-07 18:52:05 +0000

    Task 13. Insert in sorted linked list FIX case:  insert node at end 3

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -29,7 +29,7 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = new;
 		return (new);
 	}
-	while ((current->next->n < number))
+	while ((current->next->n < number) && current->next != NULL && current != NULL)
 	{
 		current = current->next;
 	}
