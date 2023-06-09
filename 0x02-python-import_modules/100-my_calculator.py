b29c2f19bce7d0f485c53cf9615e57038773f8bd
moulineE <elmahdi.mouline@live.fr>
2023-06-09 04:57:01 +0000

    Task 6. Build my own calculator! FIX: Pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
