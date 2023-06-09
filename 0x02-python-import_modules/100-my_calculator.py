9c67852e1229ace8a34a9bedd5909485a8cebd28
moulineE <elmahdi.mouline@live.fr>
2023-06-09 04:55:37 +0000

    Task 6. Build my own calculator! FIX: handeling of wrong numner of arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,10 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
