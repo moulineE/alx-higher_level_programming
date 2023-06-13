7bf2b09ae878684f1087e4f36dab5f53cd6f49b2
moulineE <elmahdi.mouline@live.fr>
2023-06-13 23:06:30 +0000

    Task 13. Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - function in C that checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to the linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *reader = *head;
+	int y, count = 0, *pal_array, i = 0;
+
+	if (*head == NULL)
+		return (1);
+	while (reader != NULL)
+	{
+		count++;
+		reader = reader->next;
+
+	}
+	if (count % 2 != 0)
+		return (0);
+	pal_array = malloc(count * sizeof(int));
+	if (pal_array == NULL)
+	{
+		free(pal_array);
+		return (0);
+	}
+	reader = *head;
+	while (reader != NULL)
+	{
+		pal_array[i] = reader->n;
+		 reader = reader->next;
+		 i++;
+	}
+	i = i - 1;
+	for (y = 0; y < i; i--)
+	{
+		if (pal_array[i] != pal_array[y])
+		{
+			free(pal_array);
+			return (0);
+		}
+		y++;
+	}
+	free(pal_array);
+	return (1);
+}
