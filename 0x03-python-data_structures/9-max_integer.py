a36a8db63b99187d4f919fb0adf7a8a6c206821a
moulineE <elmahdi.mouline@live.fr>
2023-06-13 02:13:37 +0000

    Task 9. Find the max FIX correct range error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        for i in range(len(my_list) - 1):
+        for i in range(len(my_list)):
             if my_list[i] > big:
                 big = my_list[i]
     return big
