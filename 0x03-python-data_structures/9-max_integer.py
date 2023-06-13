22851a980e5f4dfdc23ebc23f7f72d25ae1b3388
moulineE <elmahdi.mouline@live.fr>
2023-06-13 02:20:34 +0000

    Task 9. Find the max FIX case: negtive number in the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3i
 def max_integer(my_list=[]):
-    big = 0
     if len(my_list) == 0:
         return None
     else:
+        big = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > big:
                 big = my_list[i]
