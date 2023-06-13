3777089ca9b15d3b41fdd9f4018b5f8e038826c1
moulineE <elmahdi.mouline@live.fr>
2023-06-13 00:12:22 +0000

    Task 4. Replace in a copy FIX condition If idx is out of range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if isinstance(my_list, list):
         list_copy = my_list.copy()
-        if (len(my_list) - 1 <= idx) or idx < 0:
+        if (len(my_list) - 1 < idx) or idx < 0:
             return list_copy
         else:
             list_copy[idx] = element
