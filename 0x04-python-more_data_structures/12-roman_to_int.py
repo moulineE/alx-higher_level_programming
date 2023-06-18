baf0a0d14ccf4b836bef593b6639004280b8a875
moulineE <elmahdi.mouline@live.fr>
2023-06-18 15:55:28 +0000

    Task 12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sym = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    res = 0
+    pres = sym[roman_string[0]]
+    for i in roman_string:
+        if pres >= sym[i]:
+            res += sym[i]
+        elif (pres < sym[i] and ((pres == 1 and (sym[i] == 5 or 10)) or
+                                 (pres == 10 and (sym[i] == 50 or 100)) or
+                                 (pres == 100 and (sym[i] == 500 or 1000)))):
+            res += sym[i] - pres - pres
+        else:
+            res += sym[i]
+        pres = sym[i]
+    return res
