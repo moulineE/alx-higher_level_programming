b45d605093d933e97cf92b4435f5012201484ed2
moulineE <elmahdi.mouline@live.fr>
2023-06-30 16:05:48 +0000

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size):
+        """Instantiation with size
+
+        Args:
+            size (int): the square size
+        """
+    self.__size = size
