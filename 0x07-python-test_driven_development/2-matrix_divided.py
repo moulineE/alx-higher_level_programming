2eabbb188975a540299d2a8f35886397748f48e5
moulineE <elmahdi.mouline@live.fr>
2023-07-08 15:38:58 +0000

    Task 1. Divide a matrix FIX: typo in error mesg

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,7 +26,7 @@ def matrix_divided(matrix, div):
             not all((isinstance(digit, int) or isinstance(digit, float))
                     for digit in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+                        " of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not (isinstance(div, int) and not isinstance(div, float)):
