f35ec3e62f8077fbb4f13aafdaa413da065d18f6
moulineE <elmahdi.mouline@live.fr>
2023-07-11 02:00:26 +0000

    Task 0. Lookup FIX: pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """This module define a lopkup function that look for atribut and
 methods of an object"""
 
+
 def lookup(obj):
     """returns the list of available attributes and methods of an
     object"""
