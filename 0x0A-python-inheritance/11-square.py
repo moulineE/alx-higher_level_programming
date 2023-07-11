e49ea3081f9e8f0a8b230a85b79db5438f9a8bcd
moulineE <elmahdi.mouline@live.fr>
2023-07-11 07:48:09 +0100

    Task 11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module define a class Square  that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class is a subclass that represent a square"""
+
+    def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size (int) must be a private and positif integer
+
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
