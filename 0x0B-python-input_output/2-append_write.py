69553ea1fd901d0443a6314787b41c4780495c98
moulineE <elmahdi.mouline@live.fr>
2023-07-11 21:59:39 +0100

    Task 2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module define a function that appends a string at the end of a
+text file and return the number of character added"""
+
+
+def write_file(filename="", text=""):
+    """a function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+
+    Args:
+        filename (str): the filename of the text file to write in
+        text (str): the strig to add to the file 'filename'
+    Returns:
+        the number of character added
+
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
