1e7ba56ce9e746e78a210d5bd871f77df6a1adfa
moulineE <elmahdi.mouline@live.fr>
2023-07-11 21:54:07 +0100

    Task 1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module define a function that write a text file and
+return the number of character"""
+
+
+def write_file(filename="", text=""):
+    """a function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+
+    Args:
+        filename (str): the filename of the text file to write in
+        text (str): the strig to add to the file 'filename'
+
+    """
+    with open(filename, w, encoding="utf-8") as f:
+        return f.write(text)
