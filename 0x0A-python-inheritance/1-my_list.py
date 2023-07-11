e72a147e55d55049836b1d89d23f35e766d7cc59
moulineE <elmahdi.mouline@live.fr>
2023-07-11 02:41:15 +0000

    Task 1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module define a sub class that inherit a list and print it
+sorted"""
+
+
+class MyList(list):
+    """subclass that sorte a list and print it"""
+
+    def print_sorted(self):
+        """Public instance method that prints the list, but sorted"""
+        return sorted(self)
