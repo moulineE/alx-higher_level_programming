ef17771595d29f9fb885738faa7f364c5059fcac
moulineE <elmahdi.mouline@live.fr>
2023-07-13 04:00:20 +0100

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, value):
         """invert the '==' operator"""
-        return int != value
+        return self.real != value
 
     def __ne__(self, value):
         """invert the '!=' operator"""
-        return int == value
+        return self.real == value
