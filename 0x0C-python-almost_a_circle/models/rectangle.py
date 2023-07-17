b5ff5b5fed38724aff9a0cd67b864ee6a187c1a4
moulineE <elmahdi.mouline@live.fr>
2023-07-18 00:33:24 +0100

    Task 13. Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,3 +137,15 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """public method that returns the dictionary representation
+        of a Rectangle"""
+        return {
+                "x": self.x,
+                "y": self.y,
+                "id": self.id,
+                "height": self.height,
+                "width": self.width
+                }
+
