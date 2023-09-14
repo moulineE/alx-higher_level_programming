5304d2334f2b6fe0005643639de23a1646bc12b3
moulineE <elmahdi.mouline@live.fr>
2023-09-14 17:58:53 +0100

    Task 10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (a === 1 || isNaN(a)) {
+    return 1;
+  } else if (a < 0) {
+    return -1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+console.log(factorial(parseInt(process.argv[2])));
