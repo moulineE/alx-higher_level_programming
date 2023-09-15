ab71769dd8d15aa6a104eb73a34a575044631a3f
moulineE <elmahdi.mouline@live.fr>
2023-09-16 00:56:46 +0100

    Task 11. Second biggest! FIX: output the second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,12 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   let big = process.argv[2];
+  let secBig = process.argv[2];
   for (let i = 2; i < process.argv.length; i++) {
     if (process.argv[i] > big) {
+      secBig = big;
       big = process.argv[i];
     }
   }
-  console.log(big);
+  console.log(secBig);
 }
