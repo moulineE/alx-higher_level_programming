6c73c991bd4f04f3b8e82b559907c27ca85f546e
moulineE <elmahdi.mouline@live.fr>
2023-09-16 01:21:46 +0100

    Task 11. Second biggest! FIX: using a list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,6 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  let big = process.argv[2];
-  let secBig = process.argv[2];
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > big) {
-      secBig = big;
-      big = process.argv[i];
-    }
-  }
-  console.log(secBig);
+  const list = process.argv.slice(2).sort((a, b) => b - a);
+  console.log(list[1]);
 }
