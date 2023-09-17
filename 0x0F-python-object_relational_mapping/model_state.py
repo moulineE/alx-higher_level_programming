eb1d729570dd78979b1ce2a3fc3b222e75313d65
moulineE <elmahdi.mouline@live.fr>
2023-09-17 18:55:10 +0100

    Task 6. First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of a State and
+an instance Base = declarative_base()
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class definition of a State that inherits from Base
+
+    Attributes:
+        id (int): column of an auto-generated, unique integer
+        name (str): column of a string with maximum 128 characters
+
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
