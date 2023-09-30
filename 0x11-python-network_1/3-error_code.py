08bfa58112458c74fa9d541cadc9284fafe802e5
moulineE <elmahdi.mouline@live.fr>
2023-09-30 03:56:04 +0100

    Task 3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf8'))
+        except urllib.error.URLError as e:
+            print('Error code: {}'.format(e.code))
