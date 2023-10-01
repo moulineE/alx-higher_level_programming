0a4c3b7d01b3ca82e493181b9333a2029836b8cd
moulineE <elmahdi.mouline@live.fr>
2023-10-02 00:23:56 +0100

    Task 7. Error code #1 FIX: modify error handling for task req

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,10 +7,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        url = sys.argv[1]
-        r = requests.get(url)
-        r.raise_for_status()
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
         print(r.text)
-    except requests.exceptions.HTTPError:
-        print('Error code: '.format(r.status_code))
