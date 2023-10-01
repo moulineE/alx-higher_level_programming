8f84884ff21cef7dd4e5ee325fb1e9a6aebc2b2b
moulineE <elmahdi.mouline@live.fr>
2023-10-02 00:25:06 +0100

    descr update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id in the response header
+a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 """
 import requests
 import sys
