ad5167f049a9dec33b248cbd742c9e32534a4170
moulineE <elmahdi.mouline@live.fr>
2023-10-02 01:21:44 +0100

    Task 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1 and len(sys.argv[1]) < 2:
+        data = {'q': sys.argv[1,0]}
+    else:
+        data = {'q': ''}
+    r = requests.post(url, data)
+    try:
+        json = r.json()
+        if json:
+            print('[{}] {}'.format(json['id'], json['name']))
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
