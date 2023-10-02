2c4d7b9daefc17cd868eb5211aebef6ad017c3de
moulineE <elmahdi.mouline@live.fr>
2023-10-02 01:22:21 +0100

    Task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1 and len(sys.argv[1]) < 2:
-        data = {'q': sys.argv[1,0]}
+        data = {'q': sys.argv[1]}
     else:
         data = {'q': ''}
     r = requests.post(url, data)
